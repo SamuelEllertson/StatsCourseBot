d8efefe73b63007e8ae7e825b83c244a7f4db96b
Noah Stapp <noah@noahstapp.com>
2020-05-23 11:49:20 -0700

    Initial ML testing

--- dataupdater.py
+++ dataupdater.py
@@ -1,9 +1,2 @@
-
-"""This file will be run entirely independantly of the rest of the code. Its job is to scrape the catalog
+"""This file will be run entirely independently of the rest of the code. Its job is to scrape the catalog
 and any other relevant website, collection all the stats course data, and populate the database"""
-
-def main():
-    pass
-
-if __name__ == '__main__':
-    main()
