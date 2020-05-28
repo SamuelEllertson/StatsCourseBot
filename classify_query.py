8ea263d71d6ad2ca15e31b7f81ff954ebadf211b
maxbarshay <mbarshay@calpoly.edu>
2020-05-27 18:13:00 -0700

    getting glitch in classification method

--- classify_query.py
+++ classify_query.py
@@ -23,6 +23,9 @@ class Record:
         self.query = query
         self.answer = answer
 
+    def __repr__(self):
+        return str(self.query) + " | " + str(self.answer)
+
 
 # Only use intents that have at least two records
 def get_records(records):
@@ -46,19 +49,23 @@ def main():
     test = []
     corpus = {}
     stop_words = set(nltk.corpus.stopwords.words("english"))
-    nltk.download("averaged_perceptron_tagger")
-    nltk.download("tagsets")
-    nltk.download("punkt")
+    # nltk.download("averaged_perceptron_tagger")
+    # nltk.download("tagsets")
+    # nltk.download("punkt")
     punc = r"""!()-{};:'"\,<>./?@#$%^&*_~"""
     with open("query.txt") as fd:
         lines = fd.readlines()
+        #print(lines)
         lines = [x.strip().translate(str.maketrans("", "", punc)) for x in lines]
+        #print(lines)
         items = [x.split("|") for x in lines]
         items = [i for i in items if i[0] == "B4"]
-
         for item in items:
             words = item[1].split(" ")
             records.append(Record(item[1], item[2]))
+        #print(records)
+
+    validate(records)
 
     args = get_args()
     datastore = DataStore(args)
@@ -73,21 +80,92 @@ def main():
         for query in queries:
             test.append(model.extract_variables(query))
 
-    for query in test:
-        print(query[0] + ": " + model.predict_query(query[0]) + " | " + str(query[1]))
+    #print(test)
 
-    # # # Test-train split, 80% train 20% test
-    # records = get_records(records)
+    for query in test:
+        #print(query[0] + ": " + model.predict_query(query[0]))
+        # + " | " + str(query[1]))
+        pass
+
+    # # Test-train split, 80% train 20% test
+    #records = get_records(records)
+
+
+def assist_validation(my_data, model):
+    features = [model.get_features(r.query) for r in my_data]
+    #print(features)
+    vectors = []
+    # Get a corpus of every feature in the training set
+    for extracted in features:
+        for feature in extracted:
+            if feature not in model.feature_vector:
+                model.feature_vector[feature] = 0
+
+    #print(self.feature_vector)
+    #print(features)
+    # Create a feature vector from the entire corpus for each training record
+    for vector in features:
+        new_features = dict.fromkeys(model.feature_vector, 0)
+        for feature in vector.keys():
+            new_features[feature] = vector[feature]
+        # Convert to values only
+        new_features = np.array(list(new_features.values()))
+        vectors.append(new_features)
+    return vectors
+
+    
+def validate(records):
     # X_train, X_test, y_train, y_test = train_test_split(
     #     [r.query for r in records],
     #     [r.answer for r in records],
-    #     train_size=0.6,
-    #     stratify=[r.answer for r in records],
+    #     train_size=0.7
     # )
+    split = len(records) // 5
+    # print(records)
+    test = records[:split]
+    train = records[split:]
 
-    # model.fit(X_train, y_train)
+    X_train = []
+    y_train = []
 
-    # y_pred = model.predict(X_test)
+    X_test = []
+    y_test = []
+
+    args = get_args()
+    datastore = DataStore(args)
+    model = Model(args, datastore, None)
+
+    X_train = assist_validation(train, model)
+    X_test = assist_validation(test, model)
+
+    for trainrecord in train:
+        y_train.append(trainrecord.answer)
+
+    for testrecord in test:
+        y_test.append(testrecord.answer)
+
+    # print(len(X_train))
+    # print(len(y_train))
+
+            #stratify=[r.answer for r in records]
+
+    model = KNeighborsClassifier(n_neighbors = 1)
+
+    model.fit(X_train, y_train)
+
+    # print(len(X_test))
+    # print(len(y_test))
+
+    # X_test = [item.reshape(1,-1) for item in X_test1]
+
+    # print(X_test[:10])
+    # print(X_train[:10])
+    # new_X_test = []
+    # for i in range(len(X_test)):
+    #     features = np.array(list(X_test[i]))
+    #     new_X_test.append(features)
+    #     #print(features)
+    y_pred = model.predict(X_test)
 
     # print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
     # print(
