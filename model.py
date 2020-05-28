3a8816147e8cfbffbe381cad508b1b85e75992f1
Samuel Ellertson <samuelEllertson@hotmail.com>
2020-05-27 17:07:13 -0700

    created structure for responder, created QueryParameters container, updated model.py's get_intent method to be inline with responder needs

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-from queryspec import Intent
+from queryspec import Intent, QueryParameters
 import nltk
 import string
 from typing import Tuple, List
@@ -55,15 +55,9 @@ class Model:
             i += 1
         return general_query.strip(), vars
 
-    def get_intent(self, message: str) -> Intent:
-        """Takes in a raw message, and determines its intent, returning Intent.UNKNOWN
-        if it can not determine within some tolerance. It is permitted, and encouraged, to ask 
-        additional questions to determine a users intent"""
-        return Intent.UNKNOWN
+    def get_intent_and_params(self, message: str) -> Tuple[Intent, QueryParameters]:
+        """Takes in a raw message, and determines its intent and parameters, returning Intent.UNKNOWN 
+        and an empty QueryParameters() if it can not determine within some tolerance. It is permitted,
+        and encouraged, to ask additional questions to determine a users intent"""
 
-    def get_course_id(self, message: str) -> int:
-        """This method is very important, being called on all but one intents. 
-        It takes in a raw message, and determines which single course it is refering to.
-        It could be in the form of some or parts of its title, or its course id.
-        It should aggressively try its options to come up with its best guess, or None if it has no idea"""
-        return 312
+        return Intent.UNKNOWN, QueryParameters()
--- queryspec.py
+++ queryspec.py
@@ -21,4 +21,32 @@ class Intent(Enum):
     DOES_COURSE_INVOLVE_CODING      = auto()
     IS_COURSE_ELECTIVE              = auto()
 
-    FIND_COURSE_ABOUT_TOPIC         = auto() 
+    FIND_COURSE_ABOUT_TOPIC         = auto()
+
+class QueryParameters():
+
+    def __init__(self, class_id: int = None, term: str = None, professor: str = None, topic: str = None):
+        """Stores the parameters found in a users queries. All values should be normalized BEFORE being inserted."""
+        self.class_id = class_id
+        self.term = term
+        self.professor = professor
+        self.topic = topic
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(class_id={self.class_id}, term='{self.term}', professor='{self.professor}', topic='{self.topic}'')"
+
+    def require_class_id(self):
+        if self.class_id is None:
+            raise AttributeError("Class id")
+
+    def require_term(self):
+        if self.term is None:
+            raise AttributeError("Term")
+
+    def require_professor(self):
+        if self.professor is None:
+            raise AttributeError("Professor")
+
+    def require_topic(self):
+        if self.topic is None:
+            raise AttributeError("Topic")
